cea8ebc567082157bceb6bcd30677f9f2047deac
NgocLuan <2051052077luan@ou.edu.vn>
2023-09-07 21:22:58 +0700

    new

--- src/Compoments/Layout.js
+++ src/Compoments/Layout.js
@@ -5,26 +5,30 @@ import Home from './Home/Home';
 import LoginLayout from './Login/login';
 import SearchLayout from './Search/Search';
 import FeedbackLayout from './Feedback/Feedback';
+import MyUserReduce from './reducers/MyUserReduce.js';
+import cookie from "react-cookies";
 import CSCS from './CSCS.js';
+import { createContext, useReducer } from 'react';
 
+export const MyUserContext = createContext();
 export default function Layout() {
+  const [user, state] = useReducer(MyUserReduce, cookie.load('user') || null)
   return (
     <div>
-      <Router>
-        <div className="h-screen w-full">
-          <NavbarHeader />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/search" element={<SearchLayout />} />
-            <Route path="/feedback" element={<FeedbackLayout />} />
-            <Route path="/login" element={<LoginLayout />} />
-            <Route path="/api/cscs" element={<CSCS />} />
-          </Routes>
-        </div>
-      </Router>
-
+      <MyUserContext.Provider value={[user, state]}>
+        <Router>
+          <div className="h-screen w-full">
+            <NavbarHeader />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/search" element={<SearchLayout />} />
+              <Route path="/feedback" element={<FeedbackLayout />} />
+              <Route path="/login" element={<LoginLayout />} />
+              <Route path="/api/cscs" element={<CSCS />} />
+            </Routes>
+          </div>
+        </Router>
+      </MyUserContext.Provider >
     </div>
-
-
   );
 }
